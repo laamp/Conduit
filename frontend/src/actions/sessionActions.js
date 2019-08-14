7aee80a3011af502b5567d8ff56ccd02150679fa
Lance Smith <lance.matthew.smith@gmail.com>
2019-08-13 23:29:13 -0700

    Progress on react / redux setup

---
+++ frontend/src/actions/sessionActions.js
@@ -0,0 +1,14 @@
+import * as APIUtil from '../util/sessionApiUtil';
+import jwt_decode from 'jwt-decode';
+
+export const RECEIVE_USER_LOGOUT = 'RECEIVE_USER_LOGOUT';
+
+export const logoutUser = () => ({
+    type: RECEIVE_USER_LOGOUT
+});
+
+export const logout = () => {
+    localStorage.removeItem('jwtToken');
+    APIUtil.setAuthToken(false);
+    dispatchEvent(logoutUser());
+};
