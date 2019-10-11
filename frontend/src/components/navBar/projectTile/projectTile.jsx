845d7694e6a7a9a43409a2a7ffbf49d198cfbd66
Lance Smith <lance.matthew.smith@gmail.com>
2019-10-11 12:39:32 -0700

    Styling nav bar

--- frontend/src/components/navBar/projectTile/projectTile.jsx
+++ frontend/src/components/navBar/projectTile/projectTile.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
 export const ProjectTile = ({ project, numOfTasks }) => (
-    <>
+    <div className='project-tile'>
         <p>{project.title}</p>
         <p>{numOfTasks}</p>
-    </>
+    </div>
 );
