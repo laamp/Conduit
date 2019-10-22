806ba1bfc8e3697d99c6734c2bedd7017386f604
Lance Smith <lance.matthew.smith@gmail.com>
2019-10-21 23:28:54 -0700

    Context menu for task index updated; need to sort out order of event listeners

--- frontend/src/components/task/tasksIndex.jsx
+++ frontend/src/components/task/tasksIndex.jsx
@@ -5,7 +5,30 @@ class TasksIndex extends React.Component {
     constructor(props) {
         super(props);
 
+        this.state = {
+            contextMenuVisible: false,
+            contextMenuX: 0,
+            contextMenuY: 0,
+            task: null
+        };
+
+        this.resetContextMenu = () => {
+            this.resetState();
+        };
+
+        document.addEventListener('click', this.resetContextMenu);
+        document.addEventListener('contextmenu', this.resetContextMenu);
+
         this.moveTask = this.moveTask.bind(this);
+    }
+
+    resetState() {
+        this.setState({
+            contextMenuVisible: false,
+            contextMenuX: 0,
+            contextMenuY: 0,
+            task: null
+        });
     }
 
     taskChecked(task) {
@@ -16,47 +39,56 @@ class TasksIndex extends React.Component {
         };
     }
 
-    moveTask(task) {
+    moveTask(task, projectId) {
         // send the task and new project id here
+        // change the project id of the task
+        let movedTask = Object.assign({}, task);
+        movedTask.project = projectId;
+
+        // send the updated task to the action to send it to the backend
+        this.props.updateTask(movedTask);
+
+        this.resetState();
+    }
+
+    deleteTask() {
+        this.props.deleteTask(this.state.task._id);
+        this.resetState();
+    }
+
+    renderOtherProjects() {
+        if (!this.props.projects || !this.state.task) return null;
+
+        return (<ul>
+            {this.props.currentProjectId !== 'inbox' ?
+                <li key='project-inbox'
+                    onClick={() => this.moveTask(this.state.task, 'inbox')}>
+                    Inbox
+                </li> :
+                null}
+            {Object.values(this.props.projects).map((project, i) => {
+                if (this.state.task.project !== project._id) {
+                    return <li key={`project-${i}`}
+                        onClick={() => this.moveTask(this.state.task, project._id)}>
+                        {project.title}
+                    </li>
+                } else { return null; }
+            })}
+        </ul>);
+    }
+
+    taskContextMenu(task) {
         return e => {
-            // change the project id of the task
-            let movedTask = Object.assign({}, task);
-            movedTask.project = e.target.value;
+            e.preventDefault();
+            e.persist();
 
-            // send the updated task to the action to send it to the backend
-            this.props.updateTask(movedTask);
-            let selectors = document.querySelectorAll('.projectSelector');
-            selectors.forEach(s => s.value = '');
+            this.setState({
+                contextMenuVisible: true,
+                contextMenuX: e.clientX,
+                contextMenuY: e.clientY,
+                task
+            });
         };
-    }
-
-    deleteTask(task) {
-        this.props.deleteTask(task._id);
-    }
-
-    projectSelection(task) {
-        if (!this.props.projects) return null;
-
-        return (
-            <select className='projectSelector' defaultValue="" onChange={this.moveTask(task)}>
-                <option value="" disabled>Move to different project</option>
-
-                {this.props.currentProjectId !== 'inbox' ?
-                    <option value="inbox">Move to inbox</option> :
-                    null}
-
-                {Object.values(this.props.projects).map((project, i) => {
-                    if (task.project !== project._id) {
-                        return (
-                            <option key={`project-${i}`}
-                                value={project._id}>
-                                {project.title}
-                            </option>
-                        );
-                    } else { return null; }
-                })}
-            </select>
-        );
     }
 
     renderTasks(tasks) {
@@ -71,12 +103,10 @@ class TasksIndex extends React.Component {
                     }
 
                     return (
-                        <li key={`task-${i}`} className={status}>
+                        <li key={`task-${i}`} className={status} onContextMenu={this.taskContextMenu(task)}>
                             <input type="checkbox" onChange={this.taskChecked(task)} checked={checked} />
                             <p className='task-title'>{task.title}</p>
                             <p className='task-description'>{task.description}</p>
-                            {/* {this.projectSelection(task)} */}
-                            {/* <button onClick={() => this.deleteTask(task)}>Delete</button> */}
                         </li>
                     );
                 })}
@@ -85,16 +115,44 @@ class TasksIndex extends React.Component {
     }
 
     render() {
+        let contextMenu;
+        if (this.state.contextMenuVisible) {
+            const position = {
+                left: this.state.contextMenuX,
+                top: this.state.contextMenuY
+            };
+
+            contextMenu = <div className='cm-custom' style={position}>
+                <h1>this is a context menu</h1>
+                {this.renderOtherProjects()}
+                <div onClick={() => this.props.deleteTask(this.state.task._id)}>
+                    Delete task
+                </div>
+            </div>;
+        } else {
+            contextMenu = null;
+        }
+
         let inboxTasks = Object.values(this.props.tasks).filter(task => !task.project);
         if (this.props.currentProjectId === 'inbox') {
-            return this.renderTasks(inboxTasks);
+            return (
+                <>
+                    {this.renderTasks(inboxTasks)}
+                    {contextMenu}
+                </>
+            );
         }
 
         if (this.props.tasks && this.props.currentProjectId) {
             let currProjectTasks = Object.values(this.props.tasks).filter(task => {
                 return task.project === this.props.currentProjectId;
             });
-            return this.renderTasks(currProjectTasks);
+            return (
+                <>
+                    {this.renderTasks(currProjectTasks)}
+                    {contextMenu}
+                </>
+            );
         }
     }
 }
