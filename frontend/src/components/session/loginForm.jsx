2ca67366a08d353dfd2b118cbcfc2ff2af9b1e6c
Lance Smith <lance.matthew.smith@gmail.com>
2019-11-02 18:18:56 -0700

    Change onsuccess callback name

--- frontend/src/components/session/loginForm.jsx
+++ frontend/src/components/session/loginForm.jsx
@@ -21,7 +21,10 @@ class LoginForm extends React.Component {
     onSuccess(googleUser) {
         window.alert('on success fired');
         let profile = googleUser.getBasicProfile();
-        console.log(profile);
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
 
     signOut() {
@@ -49,8 +52,7 @@ class LoginForm extends React.Component {
                 var opts = {
                     width: 275,
                     height: 37,
-                    // onSuccess: this.onSuccess.bind(this)
-                    onSuccess: () => console.log('asdfasdfasdfasdf')
+                    onsuccess: this.onSuccess.bind(this)
                 };
                 window.gapi.signin2.render('google-button', opts);
             });
